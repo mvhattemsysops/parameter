973cd377c4c00a06848d8956243ad3fd9f549573
Mark van Hattem <mvhattemsysops@outlook.com>
2023-04-28 14:58:50 +0200

    met variabel aantal vars

--- parameter.ps1
+++ parameter.ps1
@@ -1,6 +1,9 @@
 #dit is een test
 #region parameter
 
-
+write-host "There are a total of $($args.count) arguments"
+for ( $i = 0; $i -lt $args.count; $i++ ) {
+    write-host "Argument  $i is $($args[$i])"
+}
 
 #endregion
